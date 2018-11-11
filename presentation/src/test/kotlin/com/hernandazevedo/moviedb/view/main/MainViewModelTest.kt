1bba4389c3ac4b3816484bbf62cd79769bcd2bb2
Hernand Azevedo <hernand.azevedo@gmail.com>
2018-11-11 19:57:27 -0200

    :white_check_mark:
    Add unit tests.
    Given the time I have to make the tests, I will just make one
    example of test for each layer.
    
    To be improved in the future.

---
+++ presentation/src/test/kotlin/com/hernandazevedo/moviedb/view/main/MainViewModelTest.kt
@@ -0,0 +1,63 @@
+package com.hernandazevedo.moviedb.view.main
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.MutableLiveData
+import com.hernandazevedo.moviedb.Movie
+import com.hernandazevedo.moviedb.domain.usecase.base.BaseRequestValues
+import com.hernandazevedo.moviedb.domain.usecase.base.BaseUseCase
+import com.hernandazevedo.moviedb.domain.usecase.request.SearchMovieRequest
+import com.hernandazevedo.moviedb.rx.RxImmediateSchedulerRule
+import com.hernandazevedo.moviedb.view.base.Resource
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.given
+import com.nhaarman.mockito_kotlin.mock
+import io.reactivex.Observable
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class MainViewModelTest {
+    private val searchMovieUseCase: BaseUseCase<SearchMovieRequest, List<Movie>> = mock()
+    private val fetchMyFavoritesUseCase: BaseUseCase<BaseRequestValues, List<Movie>> = mock()
+
+    private lateinit var mainViewModel: MainViewModel
+
+    @Rule
+    @JvmField
+    var rxSchedulersOverrideRule = RxImmediateSchedulerRule()
+
+    private val fakeTitleSearch = "anyTitle"
+    private val fakeMovieList: List<Movie> = listOf(
+        Movie(imdbID = "imdbID",
+            title = "title",
+            year = "1999",
+            type = "movie",
+            posterUrl = "posterURL")
+    )
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        mainViewModel = MainViewModel(
+            searchMovieUseCase,
+            fetchMyFavoritesUseCase)
+
+        given(searchMovieUseCase.executeUseCase(any())).willReturn(Observable.just(fakeMovieList))
+        given(searchMovieUseCase.run()).willReturn(Observable.just(fakeMovieList))
+
+        given(fetchMyFavoritesUseCase.executeUseCase(any())).willReturn(Observable.just(fakeMovieList))
+        given(fetchMyFavoritesUseCase.run()).willReturn(Observable.just(fakeMovieList))
+    }
+
+    @Test
+    fun shouldSearchMovieTest() {
+        val expectedResult: MutableLiveData<com.hernandazevedo.moviedb.view.base.Resource<List<Movie>>> = MutableLiveData()
+        expectedResult.value = Resource.success(fakeMovieList)
+        mainViewModel.searchMovie(fakeTitleSearch)
+        Assert.assertEquals(expectedResult.value, mainViewModel.responseSearchMovie.value)
+    }
+}
